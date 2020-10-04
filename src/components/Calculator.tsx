666e915089af0ccf185ed5ba5cac09e1968157c1
RTUPKALENKO <rtupk@softserveinc.com>
2020-10-05 00:30:39 +0300

    refactored

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,16 +1,12 @@
-import React, { Component, SyntheticEvent } from 'react';
+import React, { Component} from 'react';
 import '../index.css';
-import firebase from "firebase/app";
 import "firebase/database";
 import {
-
-  FirebaseDatabaseMutation,
-  FirebaseDatabaseNode
+    FirebaseDatabaseMutation,
 } from "@react-firebase/database";
-import { Alert } from 'reactstrap';
 
 enum Mutation {
-    ADD, 
+    ADD,
     SUB,
     MUL,
     DIV,
@@ -19,57 +15,57 @@ enum Mutation {
 }
 
 class MutationWrapper {
-    private _mutation? : Mutation;
-    
+    private _mutation?: Mutation;
+
     get mutation(): Mutation {
         return this._mutation as Mutation;
     }
-    
-    private _func? : (a : number, b : number) => number | null
 
-    get func():  (a : number, b : number) => number {
-        return this._func as  (a : number, b : number) => number;
+    private _func?: (a: number, b: number) => number | null
+
+    get func(): (a: number, b: number) => number {
+        return this._func as (a: number, b: number) => number;
     }
-    
-    private _str? : string;
+
+    private _str?: string;
 
     get str(): string {
         return this._str as string;
     }
-    
-    constructor(mutation :  Mutation) {
+
+    constructor(mutation: Mutation) {
         this._mutation = mutation
         this.init(mutation)
-    }   
+    }
 
-    private init(mutation : Mutation) : void{
-        switch(mutation) {
-            case Mutation.ADD : {
+    private init(mutation: Mutation): void {
+        switch (mutation) {
+            case Mutation.ADD: {
                 this._func = (a, b) => a + b;
                 this._str = "+";
                 break;
             }
-            case Mutation.SUB : {
+            case Mutation.SUB: {
                 this._func = (a, b) => a - b;
                 this._str = "-";
                 break;
             }
-            case Mutation.MUL : {
+            case Mutation.MUL: {
                 this._func = (a, b) => a * b;
                 this._str = "*";
                 break;
             }
-            case Mutation.DIV : {
+            case Mutation.DIV: {
                 this._func = (a, b) => a / b;
                 this._str = "/";
                 break;
             }
-            case Mutation.LEFTB : {
+            case Mutation.LEFTB: {
                 this._func = (a, b) => null;
                 this._str = "(";
                 break;
             }
-            case Mutation.RIGHTB : {
+            case Mutation.RIGHTB: {
                 this._func = (a, b) => null;
                 this._str = ")";
                 break;
@@ -77,32 +73,31 @@ class MutationWrapper {
         }
     }
 }
+
 type Record = {
-    key? : string;
-    timestamp : number;
-    equation : string;
-    result : number
+    key?: string;
+    timestamp: number;
+    equation: string;
+    result: number
 }
 
 type CalculatorState = {
-    inputStr : string;
-    latestInputStr? : string;
-    error? : Error | null;
-    pushedRecord? : Record
-    shouldPush : boolean
+    inputStr: string;
+    latestInputStr?: string;
+    error?: Error | null;
+    pushedRecord?: Record
 }
 
 class Calculator extends Component<{}, CalculatorState> {
     state: Readonly<CalculatorState> = {
-        inputStr : "",
-        latestInputStr : "",
-        error : null,
-        shouldPush : false
+        inputStr: "",
+        latestInputStr: "",
+        error: null
     };
 
     handleNumberClick(item: React.MouseEvent<HTMLButtonElement>) {
         item.preventDefault()
-        let digit : string = item.currentTarget.textContent as string;
+        let digit: string = item.currentTarget.textContent as string;
         if (digit) {
             this.setState((prevState, props) => ({
                 inputStr: prevState.inputStr + digit
@@ -113,13 +108,13 @@ class Calculator extends Component<{}, CalculatorState> {
     }
 
     handleMutationClick(item: MutationWrapper) {
-        let predicate =  item.mutation === Mutation.LEFTB
-                                || item.mutation === Mutation.RIGHTB
+        let predicate = item.mutation === Mutation.LEFTB
+            || item.mutation === Mutation.RIGHTB
         if (this.state.inputStr || predicate) {
             let mut = ['+', '-', '*', '/']
             let length = this.state.inputStr.length
             let latestStr = this.state.inputStr[length - 1]
-    
+
             if (!mut.includes(latestStr) || predicate) {
                 this.setState((prevState, props) => ({
                     inputStr: prevState.inputStr + item.str
@@ -135,8 +130,8 @@ class Calculator extends Component<{}, CalculatorState> {
         if (prevStr) {
             this.setState((prevState, props) => ({
                 inputStr: prevStr.length > 1 ?
-                          prevStr.substring(0, prevStr.length - 1)
-                          : ""
+                    prevStr.substring(0, prevStr.length - 1)
+                    : ""
             }), () => {
                 console.log(this.state)
             })
@@ -145,27 +140,23 @@ class Calculator extends Component<{}, CalculatorState> {
 
     handleClearClick() {
         this.setState({
-            inputStr : ""
+            inputStr: ""
         })
     }
 
     handleEqClick() {
         let res = ''
         try {
-         res = eval(this.state.inputStr) as string
-        
+            res = eval(this.state.inputStr) as string
+
             this.setState({
-                inputStr : res,
-                latestInputStr : this.state.inputStr,
-                error : null,
-                shouldPush: true
-            }, () => {
-                console.log("state after eq : " + this.state.shouldPush)
+                inputStr: res,
+                latestInputStr: this.state.inputStr,
+                error: null
             })
         } catch (e) {
-            console.log("ERRORR")
             this.setState({
-                error : e
+                error: e
             })
         }
     }
@@ -174,75 +165,74 @@ class Calculator extends Component<{}, CalculatorState> {
         return (<>
             <div className="container">
                 <div className={this.state.error ? "input-error" : "input-wrapper"}>
-                <input type="text" required disabled={false} value={this.state.inputStr}/>
+                    <input type="text" required disabled={false} value={this.state.inputStr} />
                 </div>
                 <div className="calc">
                     <div className="calc-item"></div>
                     <div className="calc-item" onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.LEFTB))}>(</div>
                     <div className="calc-item" onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.RIGHTB))}>)</div>
                     <div className="calc-item calc-op"
-                    onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.ADD))}>+
+                        onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.ADD))}>+
                     </div>
                     {
-                    [1, 2, 3].map((it) => {
-                        return <button className="calc-item" onClick={this.handleNumberClick.bind(this)}>
-                            {`${it}`}</button>
-                    })
+                        [1, 2, 3].map((it) => {
+                            return <button className="calc-item" onClick={this.handleNumberClick.bind(this)}>
+                                {`${it}`}</button>
+                        })
                     }
                     <div className="calc-item calc-op"
-                    onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.SUB))}>-
+                        onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.SUB))}>-
                     </div>
                     {
-                    [4, 5, 6].map((it) => {
-                        return <button className="calc-item" onClick={this.handleNumberClick.bind(this)}>
-                            {`${it}`}</button>
-                    })
+                        [4, 5, 6].map((it) => {
+                            return <button className="calc-item" onClick={this.handleNumberClick.bind(this)}>
+                                {`${it}`}</button>
+                        })
                     }
                     <div className="calc-item calc-op"
-                    onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.MUL))}>*
+                        onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.MUL))}>*
                     </div>
                     {
-                    [7, 8, 9].map((it) => {
-                        return <button className="calc-item" onClick={this.handleNumberClick.bind(this)}>
-                            {`${it}`}</button>
-                    })
+                        [7, 8, 9].map((it) => {
+                            return <button className="calc-item" onClick={this.handleNumberClick.bind(this)}>
+                                {`${it}`}</button>
+                        })
                     }
                     <div className="calc-item calc-op"
-                    onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.DIV))}>/
+                        onClick={() => this.handleMutationClick(new MutationWrapper(Mutation.DIV))}>/
                     </div>
                     <button className="calc-item" onClick={this.handleNumberClick.bind(this)}> 0</button>
                     <button className="calc-item calc-op" onClick={this.handleClearClick.bind(this)}>C</button>
                     <div className="calc-item calc-op" onClick={this.handleRmClick.bind(this)}>DEL</div>
-                    <div className="calc-item calc-op" onClick = {this.handleEqClick.bind(this)}>=</div> 
+                    <div className="calc-item calc-op" onClick={this.handleEqClick.bind(this)}>=</div>
                 </div>
                 <FirebaseDatabaseMutation type="push" path={"records/saved-records"}>
                     {({ runMutation }) => {
                         return (
                             <button
-                            className="push-btn"
-                            //disabled={this.state.shouldPush === false ? true : false}
-                            onClick={async () => {
-                                const record : Record = {
-                                    timestamp: Date.now(),
-                                    equation : this.state.latestInputStr as string,
-                                    result : +this.state.inputStr
-                                }
+                                className="push-btn"
+                                onClick={async () => {
+                                    const record: Record = {
+                                        timestamp: Date.now(),
+                                        equation: this.state.latestInputStr as string,
+                                        result: +this.state.inputStr
+                                    }
 
-                                const { key } = await runMutation(record);
-                                record.key = key as string
-                                this.setState({pushedRecord : record, shouldPush : false})
-                            }}>
-                            Push
+                                    const { key } = await runMutation(record);
+                                    record.key = key as string
+                                    this.setState({ pushedRecord: record })
+                                }}>
+                                Push
                             </button>
                         );
                     }}
-                </FirebaseDatabaseMutation>   
+                </FirebaseDatabaseMutation>
                 <div className="alert-scs" hidden={this.state.pushedRecord ? false : true}>
                     {`Record pushed :` +
-                    `\nKey ${this.state.pushedRecord?.key}\n` +
-                    `\nTimestamp  ${this.state.pushedRecord?.timestamp}`}
-                </div>      
-        </div>
+                        `\nKey ${this.state.pushedRecord?.key}\n` +
+                        `\nTimestamp  ${this.state.pushedRecord?.timestamp}`}
+                </div>
+            </div>
         </>)
     }
 }
